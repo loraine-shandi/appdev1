cae6606a77d8684ce1cd0e2f39fc942b467d0720
loraine-shandi <shandiloraine@gmail.com>
2025-08-27 00:18:56 +0800

    Activity 10: Add React About Me project

---
+++ 10_getting-started-react/src/App.jsx
@@ -0,0 +1,29 @@
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <div className="my-container">
+        <div className="card">
+          <div className='card-image'>
+            <img src="download.jpg" alt="Profile" />
+          </div>
+
+          <div className='card-content'>
+          <h1>ABOUT ME</h1>
+          <h3><strong> Name:</strong> Shandi Loraine Belen</h3>
+          <h3><strong> Program & Year:</strong> Bachelor of Science in Information Systems</h3>
+          <h3><strong>Fun Fact:</strong> I laugh at my own jokes. I always laugh. I love laughing. </h3>
+          <p>
+            I want to learn React because I heard it is one of the most popular frontend frameworks today. Learning this could help me contribute in our group project and future capstone project.
+          </p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
