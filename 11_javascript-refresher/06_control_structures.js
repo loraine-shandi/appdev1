d987a32f1557515000f58735c5b35bac680289ba
loraine-shandi <shandiloraine@gmail.com>
2025-09-05 23:19:35 +0800

    Add control structures activity

---
+++ 11_javascript-refresher/06_control_structures.js
@@ -0,0 +1,22 @@
+const grade = 79
+
+if (grade>=90){
+    console.log("Grade: A")
+} else if (grade>=80){
+    console.log("Grade: B")
+} else if (grade>=70){
+    console.log("Grade: C")
+} else {
+    console.log("Grade: F")
+}
+
+for (let i = 0; i < 5; i++){
+    console.log(i+1)
+}
+
+
+let count = 3;
+while (count > 0) {
+  console.log("Hello");
+  count -= 1
+} 
