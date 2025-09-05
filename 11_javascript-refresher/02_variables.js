5a1f35d9c462ed80249fc77a50e2daff5c5f91db
loraine-shandi <shandiloraine@gmail.com>
2025-09-05 23:12:50 +0800

    Add variables activity

---
+++ 11_javascript-refresher/02_variables.js
@@ -0,0 +1,17 @@
+let name = "shandong";
+let age = 19;
+let isStudent = true;
+
+console.log (name, age, isStudent);
+console.log (typeof name, typeof age, typeof isStudent);
+
+let x = 12;
+let y = 8;
+
+console.log("Add:", x + y);
+console.log("Subtract:", x - y);
+console.log("Multiply:", x * y);
+console.log("Divide:", x / y);
+
+console.log( "5" == 5);   
+console.log("5" === 5); 
