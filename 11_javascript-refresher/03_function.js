7f6f2e3c2541e0194bbf019c179a97ca83bad114
loraine-shandi <shandiloraine@gmail.com>
2025-09-05 23:13:33 +0800

    Add functions activity

---
+++ 11_javascript-refresher/03_function.js
@@ -0,0 +1,17 @@
+function greet(name) {
+    return console.log (`good day ${name}`) 
+}
+
+const square = (num) => {
+    console.log (`Square: ${num**2}`) 
+    
+}
+
+const calculator = (a,b) => {
+    console.log (`sum: ${a+b}`)
+    console.log (`product: ${a*b}`)
+}
+
+greet("shandong")
+square(12)
+calculator(2,4)
