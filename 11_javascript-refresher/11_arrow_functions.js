5aa9e88eb45de9f22473a09e60f9b9daaad10e82
loraine-shandi <shandiloraine@gmail.com>
2025-09-06 01:13:13 +0800

    Add arrow functions activity

---
+++ 11_javascript-refresher/11_arrow_functions.js
@@ -0,0 +1,17 @@
+const greet = (name) =>{
+    return "Hello, " + name
+}
+
+console.log(greet("Shandi"))
+
+const square = (n) => {
+    return n * n
+}
+
+console.log(square(5))
+
+const sayHi = () =>{
+    console.log("Hi!")
+}
+
+sayHi()
