f8bc1e00d8b8bfd0c5fecca955e10b832fc9bacb
loraine-shandi <shandiloraine@gmail.com>
2025-09-20 00:09:53 +0800

    Done responding to events / using hooks

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -3,25 +3,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const products = [
-  { title: 'Cabbage', isFruit: false, id: 1 },
-  { title: 'Garlic', isFruit: false, id: 2 },
-  { title: 'Apple', isFruit: true, id: 3 },
-];
-
-export default function ShoppingList() {
-  const listItems = products.map(product =>
-    <li
-      key={product.id}
-      style={{
-        color: product.isFruit ? 'magenta' : 'darkgreen'
-      }}
-    >
-      {product.title}
-    </li>
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Event Handling</h1>
+      <MyButton />
+    </div>
   );
+}
+
+function MyButton() {
+  function handleClick() {
+    alert('You clicked me!');
+  }
 
   return (
-    <ul>{listItems}</ul>
+    <button onClick={handleClick}>
+      Click me
+    </button>
   );
 }
