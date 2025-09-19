4b8ae8ae1b822a1ee27d2ccef5a2d0c205982112
loraine-shandi <shandiloraine@gmail.com>
2025-09-20 00:00:03 +0800

    Done rendering list

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -3,28 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function AdminPanel() {
-  return <h2>Welcome to the Admin Panel</h2>;
-}
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
 
-function LoginForm() {
-  return <h2>Please log in to continue</h2>;
-}
-
-export default function App() {
-  const isLoggedIn = false; 
-  let content;
-
-  if (isLoggedIn) {
-    content = <AdminPanel />;
-  } else {
-    content = <LoginForm />;
-  }
+export default function ShoppingList() {
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
+  );
 
   return (
-    <div>
-      <h1>Conditional Rendering Example</h1>
-      {content}
-    </div>
+    <ul>{listItems}</ul>
   );
 }
