a170aca300fd2aaa00ae1a45721260b1d39a3d64
loraine-shandi <shandiloraine@gmail.com>
2025-11-09 21:59:55 +0800

    Done with using a component

--- react-your-first-component/src/App.jsx
+++ react-your-first-component/src/App.jsx
@@ -1,4 +1,19 @@
+import { useState } from 'react'
+import Profile from './components/Profile'
+import Gallery from './components/Gallery'
 
+
+function App() {
+
+
+  return (
+    <>
+      <Gallery/>
+    </>
+  )
+}
+
+export default App
 
 // function App() {
 //   const [count, setCount] = useState(0)
