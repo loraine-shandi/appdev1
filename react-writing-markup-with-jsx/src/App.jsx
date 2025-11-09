b775c727ceb09654d77e8efcab401278ced32c57
loraine-shandi <shandiloraine@gmail.com>
2025-11-09 22:31:22 +0800

    Done with the rules of JSX

--- react-writing-markup-with-jsx/src/App.jsx
+++ react-writing-markup-with-jsx/src/App.jsx
@@ -4,17 +4,20 @@ function App() {
 
 
   return (
-    <h1>Hedy Lamarr's Todos</h1>
-<img 
-  src="https://i.imgur.com/yXOvdOSs.jpg" 
-  alt="Hedy Lamarr" 
-  class="photo"
->
-<ul>
-    <li>Invent new traffic lights
-    <li>Rehearse a movie scene
-    <li>Improve the spectrum technology
-</ul>
+     <>
+      <h1>Hedy Lamarr's Todos</h1>
+      <img 
+        src="https://i.imgur.com/yXOvdOSs.jpg" 
+        alt="Hedy Lamarr" 
+        className="photo" 
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve the spectrum technology</li>
+      </ul>
+    </>
+
 
   )
 }
