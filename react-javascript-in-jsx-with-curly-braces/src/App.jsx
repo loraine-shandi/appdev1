337fd5a302fba0ed0cef4a34600886b60a0d8615
loraine-shandi <shandiloraine@gmail.com>
2025-11-22 10:20:02 +0800

    Done with passing strings with quotes

---
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <Avatar />
+    </>
+  )
+}
+
+function Avatar() {
+  const avatar = 'https://i.imgur.com/7vQD0fPs.jpg';
+  const description = 'Gregorio Y. Zara';
+
+  return (
+    <img
+      className="avatar"
+      src={avatar}
+      alt={description}
+    />
+  );
+}
